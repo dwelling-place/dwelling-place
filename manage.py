715e55ab9144e8673a10c56c8b22dfd097ebc7fa
Dave Brondsema <dave@brondsema.net>
2016-11-05 20:09:16 -0400

    basic auth

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 import os
 
 from flask_script import Manager, Server
+from wsgi_basic_auth import BasicAuth
 
 from dwellingplace.settings import get_config
 from dwellingplace.app import create_app
@@ -20,6 +21,9 @@ def find_assets():
 config = get_config(os.getenv('FLASK_ENV'))
 
 app = create_app(config)
+if not os.environ.get('WSGI_AUTH_CREDENTIALS'):
+    os.environ['WSGI_AUTH_CREDENTIALS'] = 'foo:bar'
+app.wsgi_app = BasicAuth(app.wsgi_app)
 
 server = Server(host='0.0.0.0', extra_files=find_assets())
 
