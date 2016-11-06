c730ac43f33435df9596b73a9a2119ccd3fda5a7
brousch <brousch@gmail.com>
2016-11-05 20:55:16 -0400

    Moar file upload error handling

--- dwellingplace/views/index.py
+++ dwellingplace/views/index.py
@@ -1,4 +1,7 @@
+import sys
+
 import xlrd
+from xlrd.biffh import XLRDError
 
 from flask import Blueprint, request, render_template, send_file, Response
 from flask import flash, redirect, url_for
@@ -37,14 +40,26 @@ def download():
 @blueprint.route("/upload", methods=['POST'])
 def upload():
     file = request.files['file']
-    xl = xlrd.open_workbook(file_contents=file.stream.read())
     try:
-        update_structure(xl, Structure.load())
-        merge_metrics_from_dicts(parse_xlsx_into_dicts(xl))
-    except TypeError as err:
-        flash('Upload Failed: ' + err.message, "message-upload-fail")
+        xl = xlrd.open_workbook(file_contents=file.stream.read())
+    except XLRDError:
+        flash('Upload Failed: The file was probably not an Excel workbook.',
+              "message-upload-fail")
     else:
-        flash('Upload successful.', "message-upload-success")
+        try:
+            update_structure(xl, Structure.load())
+            merge_metrics_from_dicts(parse_xlsx_into_dicts(xl))
+        except TypeError as err:
+            flash('Upload Failed: ' + err.message, "message-upload-fail")
+        except KeyError:
+            flash('Upload Failed: You may have uploaded the wrong file or ' +
+                  'the format is corrupt.', "message-upload-fail")
+        except:
+            flash('Upload Failed: The reported error was: {} {}'.
+                  format(sys.exc_info()[0], sys.exc_info()[1]),
+                  "message-upload-fail")
+        else:
+            flash('Upload successful.', "message-upload-success")
     return redirect(url_for('index.get'))
 
 
